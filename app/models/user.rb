4f3b5572a9ba264fc0877ba3383df949e6ad8cb8
wintan1418 <wintan1418@gmail.com>
2022-10-27 11:12:34 +0100

    add model validations in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:google_oauth2]
+    
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.full_name = auth.info.name 
+      user.avatat_url = auth.info.image
+    end
+  end
 end
